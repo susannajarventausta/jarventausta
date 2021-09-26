c3dc9efc276e0bc28d1a757b36a093af2486fb7b
Suskiiii <susannah.jarventausta@gmail.com>
2021-09-26 11:24:20 +0300

    create-react-app

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.css"
+
+function App() {
+  return (
+    <div className='container'>
+      <p className='moi'>Moi!</p>
+    </div>
+  )
+}
+
+export default App
