1ec6b375a8d2a9e8f66aea62702cea56e3bf6e9e
Aleksi J <aksu.j@hotmail.com>
2021-10-09 09:06:18 +0300

    Add prettier config.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,19 +1,19 @@
-import React from "react"
-import "../styles/Footer.css"
+import React from "react";
+import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <div className='footer'>
-      <p className='text'>This website is approved by </p>
+    <div className="footer">
+      <p className="text">This website is approved by </p>
       <a
-        target='_blank'
-        href='https://susannajarventausta.github.io/dist/index.html'
-        className='link'
+        target="_blank"
+        href="https://susannajarventausta.github.io/dist/index.html"
+        className="link"
       >
         Pesonen
       </a>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
